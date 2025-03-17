c3643e9cbf07e97d92308eab78ae97268c0e3b6c
ThanhDuw <phatducdu@gmail.com>
2025-03-17 08:59:07 +0700

    main

---
+++ Assets/Scripts/Finishline.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Finishline : MonoBehaviour
+{
+    public GameObject victoryPanel;
+    public GameObject lossPanel;
+    private void Start()
+    {
+        victoryPanel.SetActive(false);
+        lossPanel.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Kiểm tra nếu xe có tag "Player"
+        {
+            Time.timeScale = 0f;
+            victoryPanel.SetActive(true);
+        }
+        else if (other.CompareTag("Enemy")) // Kiểm tra nếu xe có tag "Player"
+        {
+            
+            Time.timeScale = 0f;
+            lossPanel.SetActive(true);
+        }
+    }
+}
