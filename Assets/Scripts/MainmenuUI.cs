fc39a832bc6d7e6856401a231e276b7151238009
ThanhDuw <phatducdu@gmail.com>
2025-03-19 10:46:49 +0700

    main

---
+++ Assets/Scripts/MainmenuUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainmenuUI : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Scene1"); 
+    }
+
+    // Hàm xử lý khi nhấn nút Exit
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game đã thoát!"); // Chỉ hiển thị khi chạy ở chế độ Editor
+    }
+}
